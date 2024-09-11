2b69646b83e73262ae1b4aa84db4be2b449bb53b
SaulQE <saul.esquivel.028@gmail.com>
2024-09-11 14:27:21 -0500

    add controller

--- msvc-usuarios/src/main/java/com/esquivel/usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/com/esquivel/usuarios/controllers/UsuarioController.java
@@ -3,11 +3,9 @@ package com.esquivel.usuarios.controllers;
 import com.esquivel.usuarios.models.entity.Usuario;
 import com.esquivel.usuarios.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +26,32 @@ public class UsuarioController {
         Optional<Usuario> usuarioOptional = usuarioService.findById(id);
         if (usuarioOptional.isPresent()) {
             return ResponseEntity.ok(usuarioOptional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario save(Usuario usuario) {
+        return usuarioService.save(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> edit(@RequestBody Usuario newUsuario, @PathVariable Long id) {
+        Optional<Usuario> usuarioOptional = usuarioService.findById(id);
+        if (usuarioOptional.isPresent()) {
+            newUsuario.setId(id);
+            return ResponseEntity.ok(usuarioService.update(newUsuario));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Optional<Usuario> usuarioOptional = usuarioService.findById(id);
+        if (usuarioOptional.isPresent()) {
+            usuarioService.deleteById(id);
+            return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
     }
