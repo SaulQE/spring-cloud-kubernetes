cb2921c42e7251531d5fadab70545f86d5013b25
SaulQE <saul.esquivel.028@gmail.com>
2024-09-12 18:11:30 -0500

    add repository

---
+++ msvc-cursos/src/main/java/com/esquivel/cursos/repositories/CursoRepository.java
@@ -0,0 +1,9 @@
+package com.esquivel.cursos.repositories;
+
+import com.esquivel.cursos.entity.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+}
